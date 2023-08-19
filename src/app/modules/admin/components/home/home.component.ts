f0ab5442ebbeb0303a2c485021989381293beeaf
yusufalperendumlu <yusufalperendumlu@gmail.com>
2023-08-19 11:36:59 +0300

    ajax

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -1,65 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, asyncScheduler } from 'rxjs';
+import { ajax } from 'rxjs/ajax'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, AfterViewInit{
+
+    name: string = "";
 
     constructor() {
-      // const observable = new Observable<any> (data => {
-      //   data.next(5);
-      //   data.next(6);
-      //   data.next(10);
-      //   data.next("complate");
-      //   data.complete(); 
-      // })
-
-      // observable.subscribe(data => {
-      //   console.log(data);
-        
-      // })
-
-      // let data: any = 'Alperen';
-      // const subject =new BehaviorSubject(data); // bir önceki veriyi yakalayarak yazdırma
-      // subject.subscribe(data => { console.log(`ObserverA ${data}`) })
-      // subject.subscribe(data => { console.log(`ObserverB ${data}`) })
-      // subject.next(3);
-      // subject.next(5);
-      // subject.subscribe(data => { console.log(`ObserverC ${data}`) } )
-      // subject.next(7);
-      // subject.next(9);
-      // subject.next('Ahmet');
-
-      // const subject = new ReplaySubject(2); // n tane önceki veriyi yakalayarak yazdırma
-      // subject.subscribe(data => { console.log(`ObserverA ${data}`) })
-      // subject.subscribe(data => { console.log(`ObserverB ${data}`) })
-      // subject.next(3);
-      // subject.next(5);
-      // subject.subscribe(data => { console.log(`ObserverC ${data}`)});
-      // subject.next(7);
-      // subject.next(9);
-      // subject.next('Ahmet');
-
-      const subject = new AsyncSubject();  // akıştaki son veriyi yazdırır tek kaide complate methodunu ister
-      subject.subscribe(data => { console.log(`ObserverA ${data}`) })
-      subject.subscribe(data => { console.log(`ObserverB ${data}`) })
-      subject.next(3);
-      subject.next(5);
-      subject.subscribe(data => { console.log(`ObserverC ${data}`)});
-      subject.next(7);
-      subject.next(9);
-      subject.complete();
-      subject.next('Ahmet');
-      
-
-
-      
      }
 
+      ngAfterViewInit(): void {
+        asyncScheduler.schedule(() => {
+          this.name = 'Ahsen';  
+        })
+      }
+
     ngOnInit(): void {
+       ajax.getJSON("https://jsonplaceholder.typicode.com/posts").subscribe(  //endpointlere istek göndermek için -> httpClient 
+        data => { console.log(data) }
+       )
+      
     }
 
 }
